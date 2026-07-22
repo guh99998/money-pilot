c9b64c5e413fd6da255e8ee8c50f5147441130eb
Gustavo Lopes <guhrlopes999@gmail.com>
2026-07-22 08:42:54 -0300

    feat: Criado repository do model Categoria

---
+++ src/main/java/com/gustavolopes/money_pilot/repository/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package com.gustavolopes.money_pilot.repository;
+
+import com.gustavolopes.money_pilot.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+}
