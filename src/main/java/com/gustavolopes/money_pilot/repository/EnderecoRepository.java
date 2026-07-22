935bdbed16a1d3c039b5243e810d4ba8c9ba0120
Gustavo Lopes <guhrlopes999@gmail.com>
2026-07-22 08:43:12 -0300

    feat: Criado repository do model Endereco

---
+++ src/main/java/com/gustavolopes/money_pilot/repository/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package com.gustavolopes.money_pilot.repository;
+
+import com.gustavolopes.money_pilot.model.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
