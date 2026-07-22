c33a8a47c7354d196826ab594a69da43df6aa81f
Gustavo Lopes <guhrlopes999@gmail.com>
2026-07-22 12:30:55 -0300

    feat: Criado controller de Categoria

---
+++ src/main/java/com/gustavolopes/money_pilot/CategoriaController.java
@@ -0,0 +1,48 @@
+package com.gustavolopes.money_pilot;
+
+import com.gustavolopes.money_pilot.dto.CategoriaRequestDTO;
+import com.gustavolopes.money_pilot.dto.CategoriaResponseDTO;
+import com.gustavolopes.money_pilot.service.CategoriaService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+    @Autowired
+    private CategoriaService service;
+
+    @GetMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    public Page<CategoriaResponseDTO> getAllCategorias(Pageable pagination) {
+        return service.getAllCategorias(pagination);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CategoriaResponseDTO getCategoriaById(@PathVariable Long id) {
+        return service.getCategoriaById(id);
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoriaResponseDTO createCategoria(@Valid @RequestBody CategoriaRequestDTO categoria) {
+        return service.createCategoria(categoria);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCategoria(@PathVariable Long id) {
+        service.deleteCategoria(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CategoriaResponseDTO updateCategoria(@PathVariable Long id, @Valid @RequestBody CategoriaRequestDTO categoria) {
+        return service.updateCategoria(id, categoria);
+    }
+}
