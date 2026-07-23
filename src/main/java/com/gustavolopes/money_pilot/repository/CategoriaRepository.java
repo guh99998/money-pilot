592feb54fb7585c5ac5aec657a2a6b0494b10849
Gustavo Lopes <guhrlopes999@gmail.com>
2026-07-23 15:02:36 -0300

    .

--- src/main/java/com/gustavolopes/money_pilot/repository/CategoriaRepository.java
+++ src/main/java/com/gustavolopes/money_pilot/repository/CategoriaRepository.java
@@ -4,4 +4,5 @@ import com.gustavolopes.money_pilot.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    Long id(Long id);
 }
