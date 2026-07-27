f5a69644a9c3cd8b7b804bcfd26af4ffdb2183f7
Gustavo Lopes <guhrlopes999@gmail.com>
2026-07-27 10:18:06 -0300

    fix: Remover metodo invalido de CategoriaRepository
    O metodo id(Long) nao seguia nenhum prefixo de query derivation do
    Spring Data e nao era usado em nenhum lugar do codigo, quebrando o
    bootstrap do repository ao conectar num banco real.

--- src/main/java/com/gustavolopes/money_pilot/repository/CategoriaRepository.java
+++ src/main/java/com/gustavolopes/money_pilot/repository/CategoriaRepository.java
@@ -4,5 +4,4 @@ import com.gustavolopes.money_pilot.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-    Long id(Long id);
 }
